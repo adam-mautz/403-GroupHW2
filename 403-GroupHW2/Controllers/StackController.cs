a14a7af90cb85be3a62f99fbfc9823faee0d1017
Adam.mautz <Adam.mautz@gmail.com>
2019-10-12 16:47:39 -0600

    Added methods for stack controller

--- 403-GroupHW2/Controllers/StackController.cs
+++ 403-GroupHW2/Controllers/StackController.cs
@@ -8,10 +8,46 @@ namespace _403_GroupHW2.Controllers
 {
     public class StackController : Controller
     {
+        Stack<string> myStack = new Stack<string>();
+
         // GET: Stack
         public ActionResult Index()
         {
             return View();
+        }
+
+       public ActionResult AddOne()
+        {
+            int iPlace;
+            iPlace = myStack.Count() + 1;
+            myStack.Push(iPlace.ToString());
+
+            return Redirect("Index");
+        }
+
+        public void AddHuge()
+        {
+            myStack.Clear();
+            for (int i = 0; i < 2000; i++)
+            {
+                int iPosition = i + 1;
+                myStack.Push("New Entry  " + iPosition.ToString());
+            }
+        }
+
+        public ActionResult Display()
+        {
+            ViewBag.Output = "<table>";
+            ViewBag.Output += "<tr>";
+            ViewBag.Output += "<th>Stack Contents</th>";
+
+            foreach (var item in myStack)
+            {
+                ViewBag.Output += "<tr><td>#" + myStack.Pop() + "</td>";
+
+            }
+            return Redirect("Index");
+
         }
     }
 }
